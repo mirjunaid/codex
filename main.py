026f231f1b2b9066406d2fcdc53ff14977f41f66
Junaid Ahmad Mir <79140912+mirjunaid@users.noreply.github.com>
2021-08-01 10:20:37 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,11 @@
-import random
-
-
-def gamewin(comp, you):
-    if comp == you:
-        return None
-    elif comp == 'r':
-        if you == 'p':
-            return True
-        elif you == 's':
-            return False
-    elif comp == 'p':
-        if you == 'r':
-            return False
-        elif you == 's':
-            return True
-    elif comp == 's':
-        if you == 'r':
-            return True
-        elif you == 'p':
-            return False
-
-
-print('Comp Turn: Rock(r) Paper(p) or Scissor(s)?')
-randNo = random.randint(1, 3)
-if randNo == 1:
-    comp = 'r'
-elif randNo == 2:
-    comp = 'p'
-elif randNo == 3:
-    comp = 's'
-
-you = input('Your Turn: Rock(r) Paper(p) or Scissor(s)?')
-a = gamewin(comp, you)
-
-print(f'Computer chose {comp}')
-print(f'You chose {you}')
-
-if a == None:
-    print('The game is tie')
-elif a:
-    print('You win')
+num = int(input('Enter your number: '))
+sum = 0
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+if num == sum:
+    print(f'{num} is an Armstrong number.')
 else:
-    print('You lose')
+    print(f'{num} is not an Armstrong number.')
